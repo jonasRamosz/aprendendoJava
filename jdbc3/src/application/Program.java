c290b409d0404efec376b20f8bfdd78fe3978383
jonas.ramos <jonas.ramos@g4flex.com.br>
2024-09-10 11:05:38 -0300

    Praticando update de dados no banco

---
+++ jdbc3/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement(
+					"UPDATE seller " + "SET BaseSalary = BaseSalary + ? " + "WHERE " + "(DepartmentId = ?)");
+			st.setDouble(1, 3500.0);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! rows affected: " + rowsAffected);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
